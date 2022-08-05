6b383e91fa244a5cd7c349c5aa1d93c6b260b7cc
leandremoritz <leandremoritz19@gmail.com>
2022-08-05 10:29:42 +0200

    SECOND commit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const con = require("../lib/dbConnection");
 const middleware = require("../middleware/auth");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 router.get("/", (req, res) => {
   try {
@@ -133,6 +135,124 @@ router.get("/users/verify", (req, res) => {
     }
   });
 });
+
+
+
+// passsword
+// FORGOT PASSWORD
+router.post("/forgetpassword", (req, res) => {
+  try {
+    let sql = "SELECT * FROM users WHERE ?";
+    let user = {
+      email: req.body.email,
+    };
+    con.query(sql, user, (err, result) => {
+      if (err) throw err;
+      if (result === 0) {
+        res.status(400), res.send("Email not found");
+      } else {
+        // Allows me to connect to the given email account || Your Email
+        const transporter = nodemailer.createTransport({
+          host: process.env.MAILERHOST,
+          port: process.env.MAILERPORT,
+          auth: {
+            user: process.env.MAILERUSER,
+            pass: process.env.MAILERPASS,
+          },
+        });
+
+        // How the email should be sent out
+        var mailData = {
+          from: process.env.MAILERUSER,
+          // Sending to the person who requested
+          to: result[0].email,
+
+          subject: "Password Reset",
+          html: `<div>
+            <h3>Hi ${result[0].full_name},</h3>
+            <br>
+            <h4>Click link below to reset your password</h4>
+            <a href="http://localhost:6969/resetPasswordw.html">
+              Click Here to Reset Password
+              user_id = ${result[0].user_id}
+            </a>
+            <br>
+            <p>For any queries feel free to contact us...</p>
+            <div>
+              Email: ${process.env.MAILERUSER}
+              <br>
+              Tel: If needed you can add this
+            <div>
+          </div>`,
+        };
+
+        // Check if email can be sent
+        // Check password and email given in .env file
+        transporter.verify((error, success) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email valid! ", success);
+          }
+        });
+
+        transporter.sendMail(mailData, (error, info) => {
+          if (error) {
+            console.log(error);
+          } else {
+            res.status(200).json({
+              status: "ok",
+              data: result,
+            });
+          }
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Rest Password Route
+
+router.put("/reset-psw/:id", (req, res) => {
+  let sql = "SELECT * FROM users WHERE ?";
+  let user = {
+    user_id: req.params.id,
+  };
+  con.query(sql, user, (err, result) => {
+    if (err) throw err;
+    if (result === 0) {
+      res.status(400), res.send("User not found");
+    } else {
+      let newPassword = `UPDATE users SET ? WHERE user_id = ${req.params.id}`;
+
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(req.body.password, salt);
+
+      const updatedPassword = {
+        full_name: result[0].full_name,
+        email: result[0].email,
+        user_type: result[0].user_type,
+        phone: result[0].phone,
+        country: result[0].country,
+        billing_address: result[0].billing_address,
+        default_shipping_address: result[0].default_shipping_address,
+
+        // Only thing im changing in table
+        password: hash,
+      };
+
+      con.query(newPassword, updatedPassword, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send("Password Updated please login");
+      });
+    }
+  });
+});
+
+
 
 
 
